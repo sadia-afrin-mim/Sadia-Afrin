97bca175b22738880b9a296b54fc1f4bd2471a1b
Sadia Afrin Mim <sadia.csedu@gmail.com>
2023-08-16 22:58:25 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ================== typing animation =================== */
+var typed = new Typed(".typing",{
+    strings:[" ","CSE Graduate","Graduate Student","Data Analyst","Graduate Teaching Assistant", "Researcher"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop: true
+})
